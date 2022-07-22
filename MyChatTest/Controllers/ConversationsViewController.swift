9c1217d54d2d7db83dfc82f1c8150e766d6f303c
mikedev33 <michaelvy365@gmail.com>
2022-07-22 19:09:53 +0300

    Add proper log out

--- MyChatTest/Controllers/ConversationsViewController.swift
+++ MyChatTest/Controllers/ConversationsViewController.swift
@@ -7,9 +7,7 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        
-        
+ 
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -21,8 +19,6 @@ class ConversationsViewController: UIViewController {
     
     private func validateAuth() {
         if FirebaseAuth.Auth.auth().currentUser == nil {
-            let testMail = FirebaseAuth.Auth.auth().currentUser?.email
-            print(testMail ?? "cat Dog")
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
